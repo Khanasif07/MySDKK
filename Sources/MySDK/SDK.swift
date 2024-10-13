d4110e8987de6ebb3347116dabbc89710b440632
Asif Khan <asif.khan@hidubai.com>
2024-10-13 13:26:19 +0400

    first commit

---
+++ Sources/MySDK/SDK.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Asif Khan on 13/10/2024.
+//
+
+import Foundation
+public struct SDK{
+    static func doSomething(){
+        print("Do Something..")
+    }
+}
